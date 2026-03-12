28b4c571a811355a178f0ef1c0f71155f373b34b
ismail antar <antartalha@gmail.com>
2026-03-11 23:49:34 -0400

    to save

--- includes/Cylinder.hpp
+++ includes/Cylinder.hpp
@@ -6,10 +6,13 @@ class Cylinder: public Objects
 private:
     Points3 center;
     Vec3    axis;
-    double  radius;
+    double  redius;
 public:
     Cylinder(Points3 _center, Vec3 _axis, double _redius);
     ~Cylinder();
+
+
+    bool    intersect(const Ray& ray, double& t_hit);
 };
 
 Cylinder::Cylinder(Points3 _center, Vec3 _axis, double _redius): center(_center), axis(_axis), redius(_redius)
@@ -18,4 +21,13 @@ Cylinder::Cylinder(Points3 _center, Vec3 _axis, double _redius): center(_center)
 
 Cylinder::~Cylinder()
 {
+}
+
+
+bool    Cylinder::intersect(const Ray& ray, double& t_hit) {
+    // x ^2 + y^2 = r^2
+    // p(t) = o + t * D
+    // p(t) = ray.origine() + t * ray.diraction
+    // a * t ^2 + b *t + c = 0
+    // delta = 
 }
